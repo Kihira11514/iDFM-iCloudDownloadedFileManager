c04869bb84aec995dab5852999888de62033f938
Kihira11514 <clasp_rift0c@icloud.com>
2026-06-05 18:25:00 +0900

    Add unit test target

---
+++ iCloudDownloadedFileManagerTests/iCloudDownloadedFileManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  iCloudDownloadedFileManagerTests.swift
+//  iCloudDownloadedFileManagerTests
+//
+//  Created by KK on 2025/03/15.
+//
+
+import Testing
+@testable import iCloudDownloadedFileManager
+
+struct iCloudDownloadedFileManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
